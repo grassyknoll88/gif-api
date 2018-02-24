879e0f7309721ee1ffc846a090dd46a818a15692
Benjamin Knoll <Knoll@qui-gon-jinn.attlocal.net>
2018-02-23 22:08:39 -0600

    gif display updates

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,28 +1,28 @@
-var showTitle = [
-	"Smallville",
-	"The Office",
-	"Cheers",
-	"Last Kingdom",
-	"Home Improvement",
-	"Friends",
-	"Boy Meets World",
-	"Frasier",
-	"The Flash",
-	"Parks and Recreation",
+var character = [
+	"Michael Scott",
+	"Dwight Schrute",
+	"Jim Halpert",
+	"Pam Beesley",
+	"Creed Bratton",
+	"Stanley Hudson",
+	"Kevin Malone",
+	"Meredith Palmer",
+	"Ryan Howard",
+	"Oscar Martinez",
 ];
 var currentGif;
 var pausedGif;
 var animatedGif;
 var staticGif;
 
-//creates buttons
+//creates buttons from array
 function createButtons() {
 	$("#TVButtons").empty();
-	for (var i = 0; i < showTitle.length; i++) {
+	for (var i = 0; i < character.length; i++) {
 		var showBtn = $("<button>")
-			.text(showTitle[i])
+			.text(character[i])
 			.addClass("showBtn")
-			.attr({ "data-name": showTitle[i] });
+			.attr({ "data-name": character[i] });
 		$("#TVButtons").append(showBtn);
 	}
 
@@ -30,10 +30,11 @@ function createButtons() {
 	$(".showBtn").on("click", function() {
 		$(".display").empty();
 
-		var thisShow = $(this).data("name");
+
+		var thisCharacter = $(this).data("name");
 		var queryURL =
-			"http://api.giphy.com/v1/gifs/search" +
-			thisShow +
+			"http://api.giphy.com/v1/gifs/search?q=" +
+			thisCharacter +
 			"&limit=10&api_key=VqbG1fTNAeDDTWPxPdaDC6nCFb09FrXG";
 		$.ajax({ url: queryURL, method: 'GET' }).then(function(giphy) {
 			currentGif = giphy.data;
@@ -41,39 +42,45 @@ function createButtons() {
 				animatedGif = value.images.original.url;
 				pausedGif = value.images.original_still.url;
 				var thisRating = value.rating;
-				//gives blank ratings 'unrated' text
 				if (thisRating == "") {
 					thisRating = "unrated";
 				}
 				var rating = $("<h5>")
 					.html("Rated: " + thisRating)
 					.addClass("ratingStyle");
-				staticGif = $("<img>")
+					staticGif = $("<img class='gif'>")
 					.attr("data-animated", animatedGif)
-					.attr("data-paused", staticGif)
-					.attr("src", staticGif)
-					.addClass("moveOnHover");
+					.attr("data-paused", pausedGif)
+					.attr("src", pausedGif)
+					.attr("data-state", "paused")
 				var fullGifDisplay = $("<button>").append(rating, staticGif);
 				$('.display').append(fullGifDisplay);
+			});
+			//animates and pauses gif on click
+			$('.gif').on("click", function() {
+			  var state = $(this).attr("data-state");
+			     if( state === "paused"){
+					$(this).attr('src', $(this).attr("data-animated"));
+					$(this).attr("data-state", "playing");
+				} else {
+					$(this).attr("src", $(this).attr("data-paused"));
+					$(this).attr("data-state", "paused");
+				}
 			});
 		});
 	});
 }
 
-//animates and pauses gif on hover
-$(document).on('mouseover', '.playOnHover', function() {
-	$(this).attr('src', $(this).data('animated'));
-});
-$(document).on('mouseleave', '.playOnHover', function() {
-	$(this).attr('src', $(this).data('paused'));
-});
-
-//sets a button from input
-$('#addShow').on('click', function() {
-	var newShow = $('#newShowInput').val().trim();
-	showTitle.push(newShow);
-	createButtons();
-	return false;
+//generates a button from input
+$("#addShow").on("click", function() {
+	if($("#newCharacterInput").val().length > 0){
+		var newCharacter = $("#newCharacterInput").val().trim();
+		character.push(newCharacter);
+		createButtons();
+		$("#nnewCharacterInput").val("");
+		return false;
+		
+	};
 });
 
 createButtons();
